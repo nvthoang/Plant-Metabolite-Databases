bf4b8cc27651f5c37ed88d0afce7426337e3da5a
Hoang Nguyen <nvthoang@gmail.com>
2019-07-28 02:38:29 +1200

    Second Commit

--- SMILE_to_Descriptor.py
+++ SMILE_to_Descriptor.py
@@ -1,10 +1,11 @@
 #Import library
 from rdkit import Chem
 from mordred import Calculator, descriptors, Lipinski
-import pandas as pd 
+import pandas as pd
+import numpy as np
 
 # Load data
-DB_SMILE = pd.read_csv('BIOFACQUIM.csv') # Inseart the name of database in csv file
+DB_SMILE = pd.read_csv('Database_SMILE.csv') # Inseart the name of database in csv file
 DB_extracted_list = DB_SMILE.values.tolist()
 DB_SMILE_list = []
 
@@ -36,4 +37,13 @@ Descriptor_full = calc.pandas(mols2) #Should run small number of compound
 Descriptor_full['SMR'] # Druglike Descriptor: nHBDon, nHBAcc, MW, SlogP, SMR, TopoPSA,                
 
 # Export to csv file
-Descriptor_full.to_csv('BIOFACQUIMz.csv', encoding='utf-8', index=False)
+Descriptor_full.to_csv('Database_Descriptor.csv', encoding='utf-8', index=False)
+
+# Load dataframe of descriptors
+DB_full_descriptor = pd.read_csv('Database_Descriptor.csv') # Inseart the name of database in csv file
+
+# Extract column in dataframe
+DB_druglike_descriptor = DB_full_descriptor[['nHBDon', 'nHBAcc', 'MW', 'SLogP', 'SMR', 'TopoPSA']]
+
+# Export to csv file
+DB_druglike_descriptor.to_csv('Database_Druglike_Descriptor.csv', encoding='utf-8', index=False)
